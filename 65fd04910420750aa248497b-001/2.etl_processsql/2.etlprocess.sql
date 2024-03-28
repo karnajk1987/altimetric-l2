08db8f92fb3892b1d625c266ccfd28789fee6733
EC2 Default User <ec2-user@ip-10-0-5-221.ec2.internal>
2024-03-28 07:43:31 +0000

    added files

--- 65fd04910420750aa248497b-001/2.etl_processsql/2.etlprocess.sql
+++ 65fd04910420750aa248497b-001/2.etl_processsql/2.etlprocess.sql
@@ -1,15 +1,11 @@
 -- Create a Snowflake stage for loading data from CSV
 CREATE OR REPLACE STAGE my_stage
-URL = 's3://your-bucket/path/to/csv/files'
-CREDENTIALS = (AWS_KEY_ID = 'your_aws_key_id' AWS_SECRET_KEY = 'your_aws_secret_key')
+URL = 'STORAGE_AUTO'
 FILE_FORMAT = (TYPE = CSV);
 
 -- Load data into staging area
-COPY INTO my_table
-FROM @my_stage/dataset.csv
-FILE_FORMAT = (TYPE = CSV)
-ON_ERROR = CONTINUE; -- Handle errors as per your requirement
-
+dataset_path = "/home/ec2-user/environment/65fd04910420750aa248497b-001/2.etl_processsql/dataset.csv"
+PUT file://{{dataset_path}} @my_stage;
 
 -- Create a target table to store the data
 CREATE OR REPLACE TABLE target_table (
@@ -23,7 +19,5 @@ SELECT PARSE_JSON(column1).customer_id AS customer_id,
        PARSE_JSON(column1).transaction_amount AS transaction_amount
 FROM (
     SELECT FLATTEN(INPUT => PARSE_JSON(column1)) AS column1
-    FROM my_table
+    FROM @my_stage/dataset.csv
 );
-
-
